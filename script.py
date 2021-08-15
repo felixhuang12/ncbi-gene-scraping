4fe7d13aa9d9c4360f2ef7488bc66f560bfd274d
Felix Huang <felixhuang12@gmail.com>
2021-08-14 21:22:12 -0400

    added functionality for counting gene frequencies

--- script.py
+++ script.py
@@ -1,29 +1,52 @@
 from bs4 import BeautifulSoup
-import requests
 import csv
 
-data = open('data.csv', 'w')
+# create primary csv file
+data = open('data.csv', 'w', newline='')
 
+# create writer and headers for data types
 csv_writer = csv.writer(data)
-csv_writer.writerow(['GeneID','Gene Symbol', 'Gene Name', 'Discontinued?'])
+csv_writer.writerow(['GeneID','Gene Symbol', 'Gene Full Name', 'Discontinued?'])
+
+# open xml file downloaded from NCBI database
 with open('gene_result.xml') as xml_file:
     soup = BeautifulSoup(xml_file, 'xml')
 
-for gene in soup.find_all('Entrezgene'):
-    # find appropriate attributes
-    geneID = gene.find('Gene-source_src-int').text
-    geneSymbol = gene.find('Gene-ref_locus').text
-    geneDesc = gene.find('Gene-ref_desc').text
+# map frequencies to genes
+def freqDict(dict):
+    with open('freq.csv', 'w', newline='') as f: 
+        writer = csv.writer(f)
+        writer.writerow(['Gene Symbol', 'Number of Occurrences'])
+        for geneSymbol, freq in dict.items():
+            writer.writerow([geneSymbol, freq])
 
-    # note genes that NCBI classifies as discontinued
-    try:
-        discont = gene.find('Entrezgene_summary').text
-        if (discont):
-            discont = 'DISCONTINUED'
-    except Exception as e:
-        discont = ''
-    print(discont)
-    csv_writer.writerow([geneID, geneSymbol, geneDesc, discont])
+# scrape data from xml file
+def scrape():
+    frequencies = {}
+    for gene in soup.find_all('Entrezgene'):
+        # desired attributes
+        geneID = gene.find('Gene-source_src-int').text
+        geneSymbol = gene.find('Gene-ref_locus').text
+        geneDesc = gene.find('Gene-ref_desc').text
 
+        # special note for the genes that NCBI classifies as discontinued
+        try:
+            discont = gene.find('Entrezgene_summary').text
+            if (discont):
+                discont = 'DISCONTINUED'
+        except Exception as e:
+            discont = ''
+
+        # counting and updating frequencies of genes
+        if(discont):
+            pass
+        elif (geneSymbol in frequencies):
+            frequencies[geneSymbol] += 1
+        else:
+            frequencies.update({geneSymbol:1})
+        
+        csv_writer.writerow([geneID, geneSymbol, geneDesc, discont])
+    freqDict(frequencies)
+
+scrape()
 data.close()
-
